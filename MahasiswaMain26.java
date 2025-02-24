550e48e25af8f2d638aba426b243430d35c9d80c
Xyrfo <rofo.anggi18@gmail.com>
2025-02-24 09:49:07 +0700

    Percobaan 3

--- MahasiswaMain26.java
+++ MahasiswaMain26.java
@@ -10,5 +10,9 @@ public class MahasiswaMain26 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa26 mhs2 = new Mahasiswa26("Annisa Nabila","2141720160", 3.25, "TI 2L" );
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
